2ec51c4498b715dfbaa5429e7c92bf4da4006f8d
smartzheng <1213952997@qq.com>
2017-12-02 20:13:45 +0800

    修改部分音乐页面

--- douban/src/main/java/com/zs/douban/view/fragment/music/MusicListFragment.java
+++ douban/src/main/java/com/zs/douban/view/fragment/music/MusicListFragment.java
@@ -2,16 +2,26 @@ package com.zs.douban.view.fragment.music;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
 import com.zs.douban.R;
+import com.zs.douban.adapter.MusicAdapter;
+import com.zs.douban.model.bean.BombMusic;
+import com.zs.douban.utils.Utils;
 import com.zs.douban.view.base.BaseListFragment;
 
 import org.json.JSONArray;
 
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.QueryListener;
@@ -21,10 +31,14 @@ import cn.bmob.v3.listener.QueryListener;
  */
 
 public class MusicListFragment extends BaseListFragment {
+    @InjectView(R.id.rv_music)
+    RecyclerView mRvMusic;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_music, null);
+        ButterKnife.inject(this, rootView);
         return rootView;
     }
 
@@ -63,6 +77,12 @@ public class MusicListFragment extends BaseListFragment {
             public void done(JSONArray ary, BmobException e) {
                 if (e == null) {
                     Log.i("bmob", "查询成功：" + ary.toString());
+                    Gson gson = new Gson();
+                    List<BombMusic> bombMusics = Utils.jsonToArrayList(ary.toString(), BombMusic.class);
+                    MusicAdapter musicAdapter= new MusicAdapter();
+                    mRvMusic.setLayoutManager(new LinearLayoutManager(MusicListFragment.this.getContext()));
+                    musicAdapter.setNewData(bombMusics);
+                    mRvMusic.setAdapter(musicAdapter);
                 } else {
                     Log.i("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
                 }
@@ -79,5 +99,11 @@ public class MusicListFragment extends BaseListFragment {
     @Override
     public void onSuccess(Object data) {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
